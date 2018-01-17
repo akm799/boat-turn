15007386c45dd1509da7e3a79dc2aed5063e21ab
Thanos Mavroidis <thanosmavroidis@Thanoss-MBP.home>
2018-01-17 21:54:24 +0000

    Added turning boat model with linear resistance proportional to the square of the speed, for high speeds

--- src/main/java/uk/co/akm/test/motion/boat/quad/TurningBoat.java
+++ src/main/java/uk/co/akm/test/motion/boat/quad/TurningBoat.java
@@ -1,4 +1,4 @@
-package uk.co.akm.test.motion.boat.linear;
+package uk.co.akm.test.motion.boat.quad;
 
 import uk.co.akm.test.motion.boat.math.TrigValues;
 import uk.co.akm.test.motion.boat.model.BoatConstants;
@@ -7,11 +7,13 @@ import uk.co.akm.test.motion.boat.phys.State;
 
 /**
  * A boat that rotates at a pre-defined, constant angular velocity and a resistance to its linear motion proportional to
- * its speed.
+ * the square of its speed, for high velocities, or proportional to its speed, for low velocities.
  *
  * Created by Thanos Mavroidis on 14/01/2018.
  */
 public final class TurningBoat extends Body {
+    private static final double V_TRANSITION = 1;
+
     private final double kLon;
     private final double kLat;
     private final double kLonReverse;
@@ -20,6 +22,8 @@ public final class TurningBoat extends Body {
     private double sina;
     private double vLon;
     private double vLat;
+    private double vLonSqSigned;
+    private double vLatSqSigned;
 
     public TurningBoat(BoatConstants constants, double omgHdn0, double hdn0, double vx0, double vy0, double x0, double y0) {
         super(omgHdn0, 0, 0, hdn0, 0, 0, vx0, vy0, 0, x0, y0, 0);
@@ -39,6 +43,8 @@ public final class TurningBoat extends Body {
         final double vy = start.vy();
         vLon =  vx*cosa + vy*sina;
         vLat = -vx*sina + vy*cosa;
+        vLonSqSigned = vLon*Math.abs(vLon);
+        vLatSqSigned = vLat*Math.abs(vLat);
     }
 
     @Override
@@ -49,11 +55,19 @@ public final class TurningBoat extends Body {
         final double kLon = (vLon >= 0 ? this.kLon : this.kLonReverse);
 
         // Evaluate the acceleration wrt the linear heading.
-        final double aLon = -kLon*vLon; // Mass is 1
-        final double aLat = -kLat*vLat; // Mass is 1
+        final double aLon = estimateLinearResistanceForce(kLon, V_TRANSITION, vLon, vLonSqSigned); // Mass is 1
+        final double aLat = estimateLinearResistanceForce(kLat, V_TRANSITION, vLat, vLatSqSigned); // Mass is 1
 
         // Rotate the acceleration wrt the linear heading an angle a (i.e. the reverse of our previous rotation) to get the acceleration wrt our coordinate system.
         ax = aLon*cosa - aLat*sina;
         ay = aLon*sina + aLat*cosa;
+    }
+
+    private double estimateLinearResistanceForce(double k, double vTransition, double v, double vSqSigned) {
+        if (v > vTransition) {
+            return -k*vSqSigned;
+        } else {
+            return -k*v;
+        }
     }
 }
