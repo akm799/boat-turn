ce4ffcf2bbdc4db925a11461d4ef32e98fdb0eb1
Thanos Mavroidis <thanosmavroidis@Thanoss-MBP.home>
2018-01-21 12:02:17 +0000

    Test re-factoring.

---
+++ src/test/java/uk/co/akm/test/motion/boat/linear/BoatSlowDownLinearTest.java
@@ -0,0 +1,43 @@
+package uk.co.akm.test.motion.boat.linear;
+
+import org.junit.Test;
+import uk.co.akm.test.motion.boat.model.BoatConstants;
+import uk.co.akm.test.motion.boat.model.impl.BoatConstantsImpl;
+import uk.co.akm.test.motion.boat.phys.UpdatableState;
+import uk.co.akm.test.motion.boat.test.BoatSlowDownTest;
+
+/**
+ * Created by Thanos Mavroidis on 21/01/2018.
+ */
+public final class BoatSlowDownLinearTest extends BoatSlowDownTest {
+
+    @Override
+    protected double computeDistanceLimit(double k, double v0) {
+        return Math.abs(v0)/k;
+    }
+
+    @Override
+    protected UpdatableState boatInstance(BoatConstants constants, double omgHdn0, double hdn0, double vx0, double vy0, double x0, double y0) {
+        return new TurningBoat(constants, 0, 0, vx0, 0, 0, 0);
+    }
+
+    @Test
+    public void shouldComeToRest() {
+        final double v0 = 10; // 36 km/h
+        testSlowDown(v0);
+    }
+
+    @Test
+    public void shouldComeToRestGoingBackwards() {
+        final double v0 = -10; // -36 km/h
+        testSlowDown(v0);
+    }
+
+    private void testSlowDown(double v0) {
+        final double kLon = 1;
+        final BoatConstants constants = new BoatConstantsImpl(kLon, 50, 10);
+
+        final double time = 600; // 10 min (enough to slow down)
+        slowDownTest(constants, v0, time);
+    }
+}
---
+++ src/test/java/uk/co/akm/test/motion/boat/test/BoatSlowDownTest.java
@@ -0,0 +1,46 @@
+package uk.co.akm.test.motion.boat.test;
+
+import org.junit.Assert;
+import uk.co.akm.test.motion.boat.model.BoatConstants;
+import uk.co.akm.test.motion.boat.phys.UpdatableState;
+import uk.co.akm.test.motion.boat.phys.Updater;
+
+/**
+ * Created by Thanos Mavroidis on 21/01/2018.
+ */
+public abstract class BoatSlowDownTest {
+    private final int nSteps = 1000000;
+    private final double distanceLimitCloseness = 0.007;
+    private final double velocityLimitCloseness = 1.0E-250;
+
+    protected abstract double computeDistanceLimit(double k, double v0);
+
+    protected abstract UpdatableState boatInstance(BoatConstants constants, double omgHdn0, double hdn0, double vx0, double vy0, double x0, double y0);
+
+    protected final void slowDownTest(BoatConstants constants, double v0, double time) {
+        final double k = (v0 >= 0 ? constants.kLon() : constants.kLonReverse());
+        final double distanceLimit = computeDistanceLimit(k, v0);
+
+        // Non-turning boat setting of from the origin with an initial speed v0 along the x-axis direction.
+        final UpdatableState underTest = boatInstance(constants, 0, 0, v0, 0, 0, 0);
+
+        // Boat slows down.
+        Updater.update(underTest, time, nSteps);
+
+        // Boat has slowed down to zero velocity.
+        final double v = Math.abs(underTest.v());
+        final double vx = Math.abs(underTest.vx());
+        Assert.assertTrue(v >= 0);
+        Assert.assertTrue(vx >= 0);
+        Assert.assertTrue(v < velocityLimitCloseness);
+        Assert.assertTrue(vx < velocityLimitCloseness);
+        Assert.assertTrue(underTest.v() * v0 >= 0); // Check that velocity direction has not changed.
+        Assert.assertTrue(underTest.vx() * v0 >= 0); // Check that velocity direction has not changed.
+
+        // Boat almost reached the theoretical distance limit.
+        final double x = Math.abs(underTest.x());
+        Assert.assertTrue(x < distanceLimit);
+        Assert.assertTrue(x > distanceLimit - distanceLimitCloseness);
+        Assert.assertTrue(underTest.x() * v0 > 0); // Check that the final x-coordinate sign is consistent with the initial velocity sign.
+    }
+}
