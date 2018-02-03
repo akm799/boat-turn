a4b6409b1969cae96ae8f52262a1a9e905bcf10c
Thanos Mavroidis <thanosmavroidis@Thanoss-MBP.home>
2018-02-03 19:16:08 +0000

    Added no rotation (turn) direction enum value.

--- src/main/java/uk/co/akm/test/motion/boat/model/Rotation.java
+++ src/main/java/uk/co/akm/test/motion/boat/model/Rotation.java
@@ -6,7 +6,7 @@ package uk.co.akm.test.motion.boat.model;
  * Created by Thanos Mavroidis on 03/02/2018.
  */
 public enum Rotation {
-    LEFT(1), RIGHT(-1);
+    LEFT(1), NONE(0), RIGHT(-1);
 
     public final int sign;
 
